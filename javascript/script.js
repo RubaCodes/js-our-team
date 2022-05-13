1a873cbb62fd15e9a8e6d5dd54ab8451286ffe7f
rubacodes <rubacodes@gmail.com>
2022-05-13 15:34:54 +0200

    inserito array con oggetti i fondatori

---
+++ javascript/script.js
@@ -0,0 +1,32 @@
+let foundingMembers = [
+    {
+        nome: 'Wayne Barret',
+        ruolo : 'Founder',
+        profilePicture : 'wayne-barnett-founder-ceo.jpg'
+    },
+    {
+        nome: 'Walter Gordon Barret',
+        ruolo : 'Office Manager',
+        profilePicture : 'walter-gordon-office-manager.jpg'
+    },
+    {
+        nome: 'Scoot Estrada',
+        ruolo : 'Developer',
+        profilePicture : 'scott-estrada-developer.jpg'
+    },
+    {
+        nome: 'Barbara Ramos',
+        ruolo : 'Graphic Designer',
+        profilePicture : 'barbara-ramos-graphic-designer.jpg'
+    },
+    {
+        nome: 'Angela Lopez',
+        ruolo : 'Social Media Manager',
+        profilePicture : 'angela-lopez-social-media-manager.jpg'
+    },
+    {
+        nome: 'Angela Carrol',
+        ruolo : 'Chief Editor',
+        profilePicture : 'angela-caroll-chief-editor.jpg'
+    }
+];
