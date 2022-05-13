843556d639d0fb047af42fba163596eb69ef9eb7
rubacodes <rubacodes@gmail.com>
2022-05-13 15:39:49 +0200

    hook a team container e commento card base

--- javascript/script.js
+++ javascript/script.js
@@ -1,32 +1,37 @@
+
+// Array di oggetti aventi le informazioni sul team originario dell'azienda
 let foundingMembers = [
     {
         nome: 'Wayne Barret',
-        ruolo : 'Founder',
-        profilePicture : 'wayne-barnett-founder-ceo.jpg'
+        ruolo: 'Founder',
+        profilePicture: 'wayne-barnett-founder-ceo.jpg'
     },
     {
         nome: 'Walter Gordon Barret',
-        ruolo : 'Office Manager',
-        profilePicture : 'walter-gordon-office-manager.jpg'
+        ruolo: 'Office Manager',
+        profilePicture: 'walter-gordon-office-manager.jpg'
     },
     {
         nome: 'Scoot Estrada',
-        ruolo : 'Developer',
-        profilePicture : 'scott-estrada-developer.jpg'
+        ruolo: 'Developer',
+        profilePicture: 'scott-estrada-developer.jpg'
     },
     {
         nome: 'Barbara Ramos',
-        ruolo : 'Graphic Designer',
-        profilePicture : 'barbara-ramos-graphic-designer.jpg'
+        ruolo: 'Graphic Designer',
+        profilePicture: 'barbara-ramos-graphic-designer.jpg'
     },
     {
         nome: 'Angela Lopez',
-        ruolo : 'Social Media Manager',
-        profilePicture : 'angela-lopez-social-media-manager.jpg'
+        ruolo: 'Social Media Manager',
+        profilePicture: 'angela-lopez-social-media-manager.jpg'
     },
     {
         nome: 'Angela Carrol',
-        ruolo : 'Chief Editor',
-        profilePicture : 'angela-caroll-chief-editor.jpg'
+        ruolo: 'Chief Editor',
+        profilePicture: 'angela-caroll-chief-editor.jpg'
     }
 ];
+
+//hook alla card dove inserire il contenuto aggiuntivo
+const teamContainer = document.querySelector('.team-container');
